081e0c64a3211055d52c9eecf50b9e86b8c89ea6
m11toh <martohmashah@gmail.com>
2024-04-15 12:57:09 +0300

    data

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const scrollReveal = document.getElementById
+const menuBtn = document.getElementById("menu-btn")
+const navLinks = document.getElementById("nav_links")
+const menuBtnIcon = menuBtn.querySelector("i")
+
+menuBtn.addEventListener("click", (e) =>{
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute(
+        "class",
+        isOpen ? "ri-close-line" : "ri-menu-2-line"
+    );
+});
